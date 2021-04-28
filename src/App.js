adac00ba36bab5bd4c0b16324fefed960af617f5
Aditee Singh <singhaditee33@gmail.com>
2021-04-28 20:15:40 +0530

    Added burger structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
-
-function App() {
-  return (
-    <div >
-      Hello
-    </div>
-  );
+import React , { Component } from 'react';
+import Layout from './Components/Layout/Layout';
+import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
